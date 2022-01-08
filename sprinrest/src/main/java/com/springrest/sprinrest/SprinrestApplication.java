1b4acf27e4826b1359b4a5e027d41c789c30021c
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2022-01-08 16:19:56 -0500

    minor change

--- sprinrest/src/main/java/com/springrest/sprinrest/SprinrestApplication.java
+++ sprinrest/src/main/java/com/springrest/sprinrest/SprinrestApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SprinrestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SprinrestApplication.class, args);
+
 	}
 
 }
